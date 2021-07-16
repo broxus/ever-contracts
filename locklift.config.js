2eb1cd25c93a040dd0fd025bccc79af5a4d48fed
Sergey P <pavlovdog.moscow@gmail.com>
2021-07-16 13:25:45 +0300

    Update dev giver

--- locklift.config.js
+++ locklift.config.js
@@ -13,7 +13,7 @@ module.exports = {
         }
       },
       giver: {
-        address: '0:6c2f2e13d566aee4cbec3ea9960184177313a9618df6237525ef5751df7ee856',
+        address: '0:28cbba1c9052a6552e600e53d57d17fa3a1f1a9a05ce1d1f5c8a825d5811811e',
         abi: { "ABI version": 2, "header": ["pubkey", "time", "expire"], "functions": [ { "name": "constructor", "inputs": [ ], "outputs": [ ] }, { "name": "sendGrams", "inputs": [ {"name":"dest","type":"address"}, {"name":"amount","type":"uint64"} ], "outputs": [ ] }, { "name": "owner", "inputs": [ ], "outputs": [ {"name":"owner","type":"uint256"} ] } ], "data": [ {"key":1,"name":"owner","type":"uint256"} ], "events": [ ] },
         key: process.env.DEV_GIVER_KEY,
       },
