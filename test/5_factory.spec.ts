a5bbd738705c35630533b9aaf9ea2a4de7f46518
Alexander <fairyfromalfeya@gmail.com>
2022-12-29 14:20:11 +0100

    feat: factory code hash and version fixes

--- test/5_factory.spec.ts
+++ test/5_factory.spec.ts
@@ -2,6 +2,9 @@ import { Address, WalletTypes, Contract, lockliftChai } from 'locklift';
 import chai, { expect } from 'chai';
 import { FactorySource } from '../build/factorySource';
 import { EmptyTvmCell } from './contants';
+import { BigNumber } from 'bignumber.js';
+
+BigNumber.config({ EXPONENTIAL_AT: 1e9 });
 
 chai.use(lockliftChai);
 
@@ -99,7 +102,16 @@ describe('Factory', () => {
         })
         .and.emit('InstanceVersionChanged')
         .count(1)
-        .withNamedArgs({ current: '1', previous: '0' });
+        .withNamedArgs({
+          current: '1',
+          previous: '0',
+          currentCodeHash: new BigNumber(
+            FactoryInstance.codeHash,
+            16,
+          ).toString(),
+          previousCodeHash:
+            '68134197439415885698044414435951397869210496020759160419881882418413283430343',
+        });
     });
 
     it('should return instance code', async () => {
@@ -119,6 +131,44 @@ describe('Factory', () => {
         .call();
 
       return expect(version.value0).to.be.equal('1');
+    });
+
+    it('should update instance code with specific version', async () => {
+      const FactoryInstance =
+        locklift.factory.getContractArtifacts('FactoryInstance');
+
+      const { traceTree } = await locklift.tracing.trace(
+        example.methods
+          .setInstanceCode({
+            _newInstanceCode: FactoryInstance.code,
+            _newInstanceVersion: '3',
+            _remainingGasTo: address,
+          })
+          .send({ amount: locklift.utils.toNano(10), from: address }),
+      );
+
+      return expect(traceTree)
+        .to.call('setInstanceCode')
+        .count(1)
+        .withNamedArgs({
+          _newInstanceCode: FactoryInstance.code,
+          _newInstanceVersion: '3',
+          _remainingGasTo: address,
+        })
+        .and.to.emit('InstanceVersionChanged')
+        .count(1)
+        .withNamedArgs({
+          current: '3',
+          previous: '1',
+          currentCodeHash: new BigNumber(
+            FactoryInstance.codeHash,
+            16,
+          ).toString(),
+          previousCodeHash: new BigNumber(
+            FactoryInstance.codeHash,
+            16,
+          ).toString(),
+        });
     });
   });
 
@@ -169,6 +219,9 @@ describe('Factory', () => {
     });
 
     it('should return InstanceDeployed event after deploy', async () => {
+      const FactoryInstance =
+        locklift.factory.getContractArtifacts('FactoryInstance');
+
       const params = await example.methods
         .getDeployParams({ _id: 0, answerId: 0 })
         .call();
@@ -185,13 +238,17 @@ describe('Factory', () => {
         instance: Address;
         deployParams: string;
         version: string;
+        codeHash: string;
         deployer: Address;
       };
 
       expect(events.events.length).to.be.equal(1);
       expect(data.instance.toString()).to.be.equal(contract.value0.toString());
       expect(data.deployParams).to.be.equal(params.value0);
-      expect(data.version).to.be.equal('1');
+      expect(data.version).to.be.equal('3');
+      expect(data.codeHash).to.be.equal(
+        new BigNumber(FactoryInstance.codeHash, 16).toString(),
+      );
       return expect(data.deployer.toString()).to.be.equal(address.toString());
     });
   });
