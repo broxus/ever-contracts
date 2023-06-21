39d177a8aa67ca125b8857727619efff8b1cefee
pavlovdog <pavlovdog.moscow@gmail.com>
2023-06-21 14:42:44 +0400

    Upgrade compiler and linker
    - Check locklift.config.ts

--- locklift.config.ts
+++ locklift.config.ts
@@ -1,44 +1,51 @@
-import { LockliftConfig } from 'locklift';
-import { FactorySource } from './build/factorySource';
-import { SimpleGiver } from './giver';
+import {LockliftConfig} from "locklift";
+import { FactorySource } from "./build/factorySource";
+const LOCAL_NETWORK_ENDPOINT = "http://localhost/graphql";
+
 
 declare global {
-  const locklift: import('locklift').Locklift<FactorySource>;
+  const locklift: import("locklift").Locklift<FactorySource>;
 }
 
-const LOCAL_NETWORK_ENDPOINT = 'http://localhost/graphql';
-
 const config: LockliftConfig = {
-  compiler: { version: '0.63.0' },
-  linker: { version: '0.18.4' },
+  compiler: {
+    version: "0.70.0",
+  },
+  linker: {
+    version: "0.20.3",
+  },
   networks: {
     local: {
+      // Specify connection settings for https://github.com/broxus/everscale-standalone-client/
       connection: {
-        id: 1337,
-        group: 'localnet',
-        type: 'graphql',
+        group: "localnet",
+        type: "graphql",
         data: {
           endpoints: [LOCAL_NETWORK_ENDPOINT],
-          latencyDetectionInterval: 1000,
           local: true,
         },
       },
+      // This giver is the default local-node giverV2
       giver: {
-        giverFactory: (ever, keyPair, address) =>
-          new SimpleGiver(ever, keyPair, address),
-        address:
-          '0:ece57bcc6c530283becbbd8a3b24d3c5987cdddc3c8b7b33be6e4a6312490415',
-        key: '172af540e43a524763dd53b26a066d472a97c4de37d5498170564510608250c3',
+        // Check if you need to provide a custom giver
+        address: "0:ece57bcc6c530283becbbd8a3b24d3c5987cdddc3c8b7b33be6e4a6312490415",
+        key: "172af540e43a524763dd53b26a066d472a97c4de37d5498170564510608250c3",
       },
-      tracing: { endpoint: LOCAL_NETWORK_ENDPOINT },
+      tracing: {
+        endpoint: LOCAL_NETWORK_ENDPOINT,
+      },
       keys: {
-        phrase:
-          'action inject penalty envelope rabbit element slim tornado dinner pizza off blood',
+        // Use everdev to generate your phrase
+        // !!! Never commit it in your repos !!!
+        // phrase: "action inject penalty envelope rabbit element slim tornado dinner pizza off blood",
         amount: 20,
       },
     },
   },
-  mocha: { timeout: 2000000 },
+  // you can use any settings that mocha framework support
+  mocha: {
+    timeout: 2000000,
+  },
 };
 
 export default config;
